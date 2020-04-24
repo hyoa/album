07085917bbbe0fe73091e509aa967483e1b8f079
Jules Matsounga <hyoakato@gmail.com>
2020-04-24 17:19:14 +0200

    fix: prebuild

--- frontend/prebuild.js
+++ frontend/prebuild.js
@@ -19,11 +19,11 @@ const firebaseWorkerFileReplaced = firebaseWorkerFile.replace(/SENDER_ID/, fireb
 fs.writeFileSync(`${__dirname}/public/firebase-messaging-sw.js`, firebaseWorkerFileReplaced)
 
 // Create .env file for production
-fs.writeFileSync(`${__dirname}/.env`, `VUE_APP_API_URI=${apiUri}\nVUE_APP_FIREBASE_SENDER_ID=${firebaseSender}\nVUE_APP_SITE_TITLE=${siteTitle}\VUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
+fs.writeFileSync(`${__dirname}/.env`, `VUE_APP_API_URI=${apiUri}\nVUE_APP_FIREBASE_SENDER_ID=${firebaseSender}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
 
 if (process.env.CONTEXT !== 'production') {
   // Create .env file for pr review
-  fs.writeFileSync(`${__dirname}/.env.e2e`, `VUE_APP_API_URI=${process.env.API_URI_DEV}\nVUE_APP_FIREBASE_SENDER_ID=${process.env.FIREBASE_SENDER_ID_DEV}\nVUE_APP_SITE_TITLE=${siteTitle}\VUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
+  fs.writeFileSync(`${__dirname}/.env.e2e`, `VUE_APP_API_URI=${process.env.API_URI_DEV}\nVUE_APP_FIREBASE_SENDER_ID=${process.env.FIREBASE_SENDER_ID_DEV}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
 
   const env = {
     'VUE_APP_API_URI': apiUri,
