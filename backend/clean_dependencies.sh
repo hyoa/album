6c7a3d71889d250207674cdb7628bb30e69f05af
Jules Matsounga <jmatsounga@gmail.com>
2020-04-24 14:33:36 +0200

    fix: try to remove deps

--- backend/clean_dependencies.sh
+++ backend/clean_dependencies.sh
@@ -2,12 +2,10 @@
 
 if [[ -z "$1" ]]
 then
-    path=$PWD
+    path=PWD
 else
     path=$1
 fi
-
-echo $path
 
 echo "Cleaning useless code from vendor"
 rm -rf $path/vendor/php-ffmpeg/php-ffmpeg/tests/
