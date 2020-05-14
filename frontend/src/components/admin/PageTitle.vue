0b7b8b65f1bd10374b900b59b3bebd4e017b3241
Jules Matsounga <hyoakato@gmail.com>
2020-05-14 10:41:56 +0200

    fix: icon to white

--- frontend/src/components/admin/PageTitle.vue
+++ frontend/src/components/admin/PageTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-white px-2 py-2 shadow-md" :class="color">
     <h1 class="text-3xl">
-      <v-icon class="icon" v-if="icon" :name="icon" scale="1.2"></v-icon>
+      <v-icon class="icon" v-if="icon" :name="icon" scale="1.2" :inverse="true"></v-icon>
       {{ title }}
     </h1>
   </div>
