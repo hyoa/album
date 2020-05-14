22cab1b5f5933a0d95daca1741ecd092614f07a3
Jules Matsounga <hyoakato@gmail.com>
2020-05-14 11:30:37 +0200

    fix: set back tailwindcss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,6 +3,7 @@ const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 module.exports = {
   plugins: [
     require('postcss-preset-env')({ stage: 0 }),
+    require('tailwindcss')(),
     require('autoprefixer')()
   ]
 }
