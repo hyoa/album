177d73eb528968050e8e9fbb3a324ae90c3775eb
Jules Matsounga <hyoakato@gmail.com>
2021-04-09 14:38:36 +0200

    fix: use independant secret

--- backend/src/Application/Helper/JWTHelper.php
+++ backend/src/Application/Helper/JWTHelper.php
@@ -9,7 +9,6 @@ use Lcobucci\Clock\SystemClock;
 use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\Signer\Key\LocalFileReference;
 use Lcobucci\JWT\Token;
 use Lcobucci\JWT\UnencryptedToken;
 use Lcobucci\JWT\Validation\Constraint\SignedWith;
