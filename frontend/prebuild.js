cf79917fd98caaa78106f1c673fabb40bb1061c0
Jules Matsounga <jules.matsounga@bedrockstreaming.com>
2022-12-19 13:02:12 +0100

    fix: add graphqluri

--- frontend/prebuild.js
+++ frontend/prebuild.js
@@ -12,14 +12,15 @@ manifestFileReplaced = manifestFileReplaced.replace(/SITE_SHORT_TITLE/, siteShor
 fs.writeFileSync(`${__dirname}/public/manifest.json`, manifestFileReplaced)
 
 // Create .env file for production
-fs.writeFileSync(`${__dirname}/.env`, `VUE_APP_API_URI=${apiUri}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
+fs.writeFileSync(`${__dirname}/.env`, `VUE_APP_API_URI=${apiUri}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}\nVUE_APP_GRAPHQL_URI=${apiUri}`)
 
 if (process.env.CONTEXT !== 'production') {
   // Create .env file for pr review
-  fs.writeFileSync(`${__dirname}/.env.e2e`, `VUE_APP_API_URI=${process.env.API_URI_DEV}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}`)
+  fs.writeFileSync(`${__dirname}/.env.e2e`, `VUE_APP_API_URI=${process.env.API_URI_DEV}\nVUE_APP_SITE_TITLE=${siteTitle}\nVUE_APP_SITE_SHORT_TITLE=${siteShortTitle}\nVUE_APP_GRAPHQL_URI=${process.env.API_URI_DEV}`)
 
   const env = {
     'VUE_APP_API_URI': apiUri,
+    'VUE_APP_GRAPHQL_URI': apiUri,
     'VUE_APP_SITE_TITLE': siteTitle,
     'VUE_APP_SITE_SHORT_TITLE': siteShortTitle
   }
