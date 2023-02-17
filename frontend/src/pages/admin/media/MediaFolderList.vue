a99e3cbe6d52f95fcff28d9e7c44d69385f73eda
Jules Matsounga <jules.matsounga@bedrockstreaming.com>
2023-02-17 13:24:24 +0100

    chore: add filter on media list

--- frontend/src/pages/admin/media/MediaFolderList.vue
+++ frontend/src/pages/admin/media/MediaFolderList.vue
@@ -3,8 +3,9 @@
     <template v-slot:title>
       <PageTitle :title="$t('admin.mediaFolderList.title')" icon="regular/plus-square" color="bg-green-500"/>
     </template>
+    <InputSimple placeholder="vacances" label="Filtre" v-model="filter" type="text"/>
     <ul>
-      <ListItem :key="folder.name" v-for="folder of folders" :to="{ name: 'admin_medias_folder', params: { folder: folder.name } }" :title="folder.name" />
+      <ListItem :key="folder.name" v-for="folder of foldersToDisplay" :to="{ name: 'admin_medias_folder', params: { folder: folder.name } }" :title="folder.name" />
     </ul>
   </AdminLayout>
 </template>
@@ -15,12 +16,15 @@ import { graphql } from '../../../utils/axiosHelper'
 import AdminLayout from '../../../components/layout/AdminLayout'
 import ListItem from '../../../components/admin/ListItem'
 import PageTitle from '../../../components/admin/PageTitle'
+import InputSimple from '../../../components/form/default/InputSimple'
+
 export default {
   name: 'MediaFolderList',
-  components: { PageTitle, ListItem, AdminLayout },
+  components: { PageTitle, ListItem, AdminLayout, InputSimple },
   data () {
     return {
-      folders: []
+      folders: [],
+      filter: ''
     }
   },
   async created () {
@@ -34,6 +38,17 @@ export default {
 
     const { folders } = await graphql(query, 'v3')
     this.folders = folders
+  },
+  computed: {
+    foldersToDisplay () {
+      if (this.filter === '') {
+        return this.folders
+      }
+
+      return this.folders.filter(folder => {
+        return folder.name.includes(this.filter)
+      })
+    }
   }
 }
 </script>
