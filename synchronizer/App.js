44c643565152680d31a8fe32c396d2c70d874406
Matsounga Jules <hyoakato@gmail.com>
2023-09-30 18:36:46 +0200

    fix: add catch when getting media info from gallery to prevent fatalerror on upload (#96)
    * fix: add catch when getting media info from gallery to prevent fatal error on upload
    
    * fix: console.log

--- synchronizer/App.js
+++ synchronizer/App.js
@@ -237,7 +237,7 @@ function HomeScreen() {
 
     const authToken = await SecureStore.getItemAsync('authToken')
 
-    try {
+    try {      
       const resp = await axios.post(
         `${API_HOST}/v3/graphql`,
         {
@@ -256,17 +256,32 @@ function HomeScreen() {
       // Create a new array with the medias to upload
 
       const limit = pLimit(5)
+
       for (let media of resp.data.data.medias) {
+          let item = items.find(({filename}) => {
+            return formatFileName(filename) === media.key
+          })
+
           const conf = mediasConfig.find(({ key }) => key === media.key)
 
-          let picture = await fetch(conf.uri)
+          let picture
+          try {
+            picture = await fetch(conf.uri)
+          } catch (e) {
+            let item = items.find(({filename}) => {
+              return formatFileName(filename) === media.key
+            })
+
+            setItems({type: 'setSyncStatus', id: item.id, 'status': 'failed'})
+
+            continue
+          }
+
           picture = await picture.blob()
 
           const mimeType = mime.lookup(media.key)
 
-          let item = items.find(({filename}) => {
-            return formatFileName(filename) === media.key
-          })
+
           setItems({type: 'setSyncStatus', id: item.id, 'status': 'inprogress'})
           
           const req = fetch(
@@ -302,7 +317,6 @@ function HomeScreen() {
       }
 
       setGlobalSyncStatusText('Envoi des médias sur le serveur')
-
 
       // Wait for all the medias to be uploaded
       // Then, create the folder and ingest the medias
@@ -381,7 +395,6 @@ function HomeScreen() {
       })
 
     } catch(e) {
-      console.log(e)
       setGlobalSyncStatus(STATUS_READY)
     }
   }
